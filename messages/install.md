705a7717a55ab0bc5ea667108d0c7ba015c5e897
Samir Ansari <developedbysam@gmail.com>
2021-05-09 18:35:34 +0545

    releasing the first version of the theme

---
+++ messages/install.md
@@ -0,0 +1,24 @@
+**Day-break theme for Sublime Text 3**
+To learn more about day-break visit https://github.com/developedby-sam/day-break-sublime-theme
+===============================================================================
+
+Activate Day-break
+
+You can also manually activate this theme by adding these lines to your user
+settings (Preferences > Settings - User):
+
+Day-break:
+
+````json
+"theme": "day-break.sublime-theme",
+"color_scheme": "Packages/Themes/day-break-sublime-theme/day-break.sublime-color-scheme",
+
+================================================================================
+
+Day-break provides following options to customize the theme
+
+```json
+"ui_native_titlebar":       true, // native titlebar on macOs
+"ui_separator":             true, // separators between panels
+"ui_wide_scrollbars":       true, // wider scrollbars
+````
---
+++ src/templates/widget.ts
@@ -0,0 +1,97 @@
+import { Scheme } from 'ayu'
+
+export default (scheme: Scheme, name: string) => `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>author</key>
+  <string>Ike Ku</string>
+  <key>colorSpaceName</key>
+  <string>sRGB</string>
+  <key>name</key>
+  <string>ayu</string>
+  <key>semanticClass</key>
+  <string>ayu.${name}</string>
+  <key>settings</key>
+  <array>
+    <dict>
+      <key>settings</key>
+      <dict>
+        <key>background</key>
+        <string>${scheme.ui.panel.bg.hex()}</string>
+        <key>caret</key>
+        <string>${scheme.common.accent.hex()}</string>
+        <key>foreground</key>
+        <string>${scheme.common.fg.hex()}</string>
+        <key>invisibles</key>
+        <string>${scheme.ui.gutter.normal.hex()}</string>
+        <key>lineHighlight</key>
+        <string>${scheme.ui.line.hex()}</string>
+        <key>inactiveSelection</key>
+        <string>${scheme.ui.selection.inactive.hex()}</string>
+        <key>selection</key>
+        <string>${scheme.ui.selection.bg.hex()}</string>
+        <key>selectionBorder</key>
+        <string>${scheme.ui.selection.border.hex()}</string>
+      </dict>
+    </dict>
+    <dict>
+      <key>name</key>
+      <string>Comments</string>
+      <key>scope</key>
+      <string>comment</string>
+      <key>settings</key>
+      <dict>
+        <key>fontStyle</key>
+        <string>italic</string>
+        <key>foreground</key>
+        <string>${scheme.syntax.comment.hex()}</string>
+      </dict>
+    </dict>
+    <dict>
+      <key>name</key>
+      <string>Keyword</string>
+      <key>scope</key>
+      <string>keyword, storage</string>
+      <key>settings</key>
+      <dict>
+        <key>foreground</key>
+        <string>${scheme.syntax.keyword.hex()}</string>
+      </dict>
+    </dict>
+    <dict>
+      <key>name</key>
+      <string>Constant</string>
+      <key>scope</key>
+      <string>constant</string>
+      <key>settings</key>
+      <dict>
+        <key>foreground</key>
+        <string>${scheme.syntax.constant.hex()}</string>
+      </dict>
+    </dict>
+    <dict>
+      <key>name</key>
+      <string>String</string>
+      <key>scope</key>
+      <string>string</string>
+      <key>settings</key>
+      <dict>
+        <key>foreground</key>
+        <string>${scheme.syntax.string.hex()}</string>
+      </dict>
+    </dict>
+    <dict>
+      <key>name</key>
+      <string>Escape</string>
+      <key>scope</key>
+      <string>constant.character.escape</string>
+      <key>settings</key>
+      <dict>
+        <key>foreground</key>
+        <string>${scheme.syntax.regexp.hex()}</string>
+      </dict>
+    </dict>
+  </array>
+</dict>
+</plist>`
